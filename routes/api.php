a12e9026baa0b6489b3c83998bb03726b442bcea
Steven Sucre <steven.g.s.p@gmail.com>
2021-08-20 02:28:04 -0400

    Updated readme and routes

--- routes/api.php
+++ routes/api.php
@@ -17,22 +17,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('tenders')->group(function () {
     // import tenders
-    Route::post('/import', [TenderController::class, 'import']);
+    Route::post('/import', [TenderController::class, 'import'])->name('tenders.import');
 
     // index of tenders
-    Route::get('/', [TenderController::class, 'index']);
+    Route::get('/', [TenderController::class, 'index'])->name('tenders.index');
 
     // show a tender
-    Route::get('/{tenderId}', [TenderController::class, 'show']);
+    Route::get('/{tenderId}', [TenderController::class, 'show'])->name('tenders.show');
 
     // check if tender was read
-    Route::get('/{tenderId}/was-read', [TenderController::class, 'wasRead']);
+    Route::get('/{tenderId}/was-read', [TenderController::class, 'wasRead'])->name('tenders.wasRead');
 });
 
 Route::prefix('uploaded-files')->group(function () {
     // index of uploaded files
-    Route::get('/', [UploadedFileController::class, 'index']);
+    Route::get('/', [UploadedFileController::class, 'index'])->name('uploadedFiles.index');
 
     // show an uploaded file
-    Route::get('/{uploadedFileId}', [UploadedFileController::class, 'show']);
+    Route::get('/{uploadedFileId}', [UploadedFileController::class, 'show'])->name('uploadedFiles.show');
 });
